cad3f25f73b82b290bd786f5671aeb8adf54740f
tulunadrockers <tulunadrockers2@gmail.com>
2026-04-27 21:31:34 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+ 
+function Home() { 
+    return ( 
+        <div className="page"> 
+            <h1>Welcome to the Home Page!</h1> 
+            <p>This is the home page of simple React application.</p> 
+        </div> 
+    ); 
+} 
+ 
+export default Home; 
