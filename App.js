e7b5790ad2b3dca2050bcf0e504d0e3543f1be07
tulunadrockers <tulunadrockers2@gmail.com>
2026-04-27 21:30:15 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import Navbar from "./Navbar"; 
+import Home from "./Home"; 
+import About from "./About"; 
+import Contact from "./Contact"; 
+ 
+ 
+function App() { 
+    return ( 
+        <BrowserRouter> 
+            <Navbar /> 
+            <Routes> 
+                <Route path="/" element={<Home />} /> 
+                <Route path="/about" element={<About />} /> 
+                <Route path="/contact" element={<Contact />} /> 
+            </Routes> 
+        </BrowserRouter> 
+    ); 
+} 
+ 
+export default App;
