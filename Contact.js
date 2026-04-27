37e2466df90fbfea8a4d16a7a8318bda896fa8d7
tulunadrockers <tulunadrockers2@gmail.com>
2026-04-27 21:31:12 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+ 
+function Contact() { 
+    return ( 
+        <div className="page"> 
+            <h1>Contact Us</h1> 
+            <p>Get in touch with us here!</p> 
+        </div> 
+    ); 
+} 
+ 
+export default Contact; 
